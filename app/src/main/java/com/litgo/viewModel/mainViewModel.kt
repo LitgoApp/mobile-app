d9e0cb76b7b6d223257ba68aec5fd54dcf2bcf44
erfan7emz <68251969+erfan7emz@users.noreply.github.com>
2023-07-15 20:29:13 -0400

    map activity

--- app/src/main/java/com/litgo/viewModel/mainViewModel.kt
+++ app/src/main/java/com/litgo/viewModel/mainViewModel.kt
@@ -9,20 +9,43 @@ import kotlinx.coroutines.launch
 
 class LitterSiteViewModel(private val repository: LitterSiteRepository) : ViewModel() {
     //decide on littercount, latitude, longitude
-    fun createLitterSite(imageUri: Uri, harm: String, description: String, litterCount: Int, latitude: Double, longitude: Double) {
+
+    fun createLitterSite(imageUri: Uri, harm: String, description: String, latitude: Double, longitude: Double) {
         val imageUriString = imageUri.toString()
 
-        val litterSiteCreation = LitterSiteCreation(
-            latitude = latitude,
-            longitude = longitude,
-            harm = harm,
-            description = description,
-            litterCount = litterCount,
-            image = imageUriString
-        )
+        // assume there is a function getLitterCount that calls the AI detector service
+        val litterCountDeferred = getLitterCount(imageUri)
 
         viewModelScope.launch {
-            repository.createLitterSite(litterSiteCreation)
+            try {
+                val litterCount = litterCountDeferred.await()
+
+                val litterSiteCreation = LitterSiteCreation(
+                    latitude = latitude,
+                    longitude = longitude,
+                    harm = harm,
+                    description = description,
+                    litterCount = litterCount,
+                    image = imageUriString
+                )
+
+                repository.createLitterSite(litterSiteCreation)
+            } catch (e: Exception) {
+                Log.e("LitterSiteViewModel", "Error creating litter site", e)
+            }
+        }
+    }
+
+    val nearbyLitterSites = MutableLiveData<List<LitterSite>>()
+
+    fun fetchNearbyLitterSites(userCoords: Coordinates) {
+        viewModelScope.launch {
+            try {
+                val litterSites = repository.getNearbyLitterSites(userCoords)
+                nearbyLitterSites.value = litterSites
+            } catch (e: Exception) {
+                Log.e("LitterSiteViewModel", "Error fetching nearby litter sites", e)
+            }
         }
     }
 }
